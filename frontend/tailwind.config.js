c4f3de1cdb959b38b5481c29271c9f72d0a97a3b
AndikaAnanda <andikaananda63@gmail.com>
2024-07-23 04:59:16 +0700

    Finishing landing page design

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ['Abril Fatface', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
